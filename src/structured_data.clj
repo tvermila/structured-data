cb1ded165e994448e01d0a443db1b1be3b8415b8
Tomas Vermilä <tomas.vermila@myy.haaga-helia.fi>
2019-07-11 15:48:05 +0300

    tehty lisää tehtäviä

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -107,31 +107,97 @@
 
 
 (defn old-book->new-book [book]
-  :-)
+  (assoc book :authors (set (:authors book))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(def china {:name "China Miéville", :birth-year 1972})
+(def octavia {:name "Octavia E. Butler"
+              :birth-year 1947
+              :death-year 2006})
+(def friedman {:name "Daniel Friedman" :birth-year 1944})
+(def felleisen {:name "Matthias Felleisen"})
+
+(def cities {:title "The City and the City" :authors #{china}})
+(def wild-seed {:title "Wild Seed", :authors #{octavia}})
+(def embassytown {:title "Embassytown", :authors #{china}})
+(def little-schemer {:title "The Little Schemer"
+                     :authors #{friedman, felleisen}})
+
+(def books [cities, wild-seed, embassytown, little-schemer])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn has-author? [book author]
-  :-)
+  (contains? (set (:authors book)) author))
 
 (defn authors [books]
-  :-)
+  (apply clojure.set/union (map :authors books)))
+
+
+(authors [cities, wild-seed])              ;=> #{china, octavia}
+(authors [cities, wild-seed, embassytown]) ;=> #{china, octavia}
+(authors [little-schemer, cities])         ;=> #{china, friedman, felleisen}
 
 (defn all-author-names [books]
-  :-)
+  (apply clojure.set/union (map :name (authors books))))
+
+(all-author-names books)
+;=> #{"Matthias Felleisen" "China Miéville"
+;     "Octavia E. Butler" "Daniel Friedman"}
+(all-author-names [cities, wild-seed])
+;=> #{"China Miéville" "Octavia E. Butler"}
+(all-author-names []) ;=> #{}
 
 (defn author->string [author]
-  :-)
+  (let [name (author :name)
+        death (author :death-year)
+        birth (author :birth-year)
+        years (str " (" birth " - " death ")")]
+      
+    (str name (if (contains? author :birth-year) years))))
+
 
 (defn authors->string [authors]
-  :-)
+  (apply str (interpose ", " (map author->string authors))))
+  
 
 (defn book->string [book]
-  :-)
+  (let [title (book :title)
+        authors (authors->string (book :authors))]
+    (str title ", written by " authors)))
+
 
 (defn books->string [books]
-  :-)
+  (let [amount (count books)]
+    (apply str 
+      (cond 
+        (= amount 0) "No books. " 
+        (= amount 1) "1 book. "
+        :else (str amount " books. "))
+      (interpose ", " (map book->string books)))))
+      
+
+
+(books->string []) ;=> "No books."
+(books->string [cities])
+;=> "1 book. The City and the City, written by China Miéville (1972 - )."
+(books->string [little-schemer, cities, wild-seed])
+;=> "3 books. The Little Schemer, written by Daniel Friedman (1944 - ), Matthias Felleisen. 
+;The City and the City, written by China Miéville (1972 - ). Wild Seed, written by Octavia E. Butler (1947 - 2006)."
+
+(has-author? cities china)             ;=> true
+(has-author? cities felleisen)         ;=> false
+(has-author? little-schemer felleisen) ;=> true
+(has-author? little-schemer friedman)  ;=> true
+(has-author? little-schemer octavia)   ;=> false
 
 (defn books-by-author [author books]
-  :-)
+  (filter (fn [book author] (contains? (:authors book)) author))) books
+
+(books-by-author china books)   ;=> (cities embassytown)
+(books-by-author octavia books) ;=> (wild-seed)
 
 (defn author-by-name [name authors]
   :-)
